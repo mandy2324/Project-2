b0e2c99ec7f5641f8be2643a00e4b53f3c223497
Cktestakis <codyktestakis@gmail.com>
2021-11-04 20:47:37 -0400

    Updated Project

--- Develop/models/Project.js
+++ Develop/models/Project.js
@@ -5,12 +5,6 @@ class Project extends Model {}
 
 Project.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -18,23 +12,8 @@ Project.init(
     description: {
       type: DataTypes.STRING,
     },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
   },
+  
   {
     sequelize,
     timestamps: false,
