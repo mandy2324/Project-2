02106023fd2a56fa23d5d98421a648d4065ee12f
unknown <jasdeepsaini30@gmail.com>
2021-11-06 13:09:57 -0400

    adding bug fixes for model

--- Develop/routes/api/user-api-routes.js
+++ Develop/routes/api/user-api-routes.js
@@ -1,13 +1,13 @@
-const User = require('./models/User.js');
+const User = require('../../models/User.js');
 
 const router = require('express').Router();
 router.post('/signup', (req, res) => {
-  User.create({ username: req.body.username, password: req.body.password })
-    .then((data) => res.json(data))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    User.create({ username: req.body.username, password: req.body.password })
+        .then((data) => res.json(data))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router
