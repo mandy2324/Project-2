c538eda350ed7e3f15cb62be2dd7dbdb8b11ab65
alexbrandon3 <alexlbrandon@gmail.com>
2021-11-09 18:42:29 -0500

    Added search and signup folders with content

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const favoriteRoutes = require("./favorite-api-routes");
+// const favoriteRoutes = require("./favorite-api-routes");
 const userApiRoutes = require("./user-api-routes");
 
-router.use("/favorites", favoriteRoutes);
+// router.use("/favorites", favoriteRoutes);
 router.use("/user", userApiRoutes);
 
 module.exports = router;
