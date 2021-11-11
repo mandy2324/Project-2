7cdd0ac0fd9a0f6b4c5e80bbdc344ed1f4299122
unknown <jasdeepsaini30@gmail.com>
2021-11-11 18:52:41 -0500

    adding handlebar fav route

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
 };
 
 app.use(session(sess));
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () =>
-    console.log(`App listening at  http://localhost:${PORT} 🚀`)
-  );
+    app.listen(PORT, () =>
+        console.log(`App listening at  http://localhost:${PORT} 🚀`)
+    );
 });
