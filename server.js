0ccb324b9b88355a49d559b696669565f9ef76f7
Cktestakis <codyktestakis@gmail.com>
2021-11-11 19:22:17 -0500

    Updated Folders

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const routes = require('./routes');
+const controllers = require('./controllers');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
