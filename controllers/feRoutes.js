7b1832f86defda6e6585b0e4351fad65bef41623
Cktestakis <codyktestakis@gmail.com>
2021-11-11 21:17:23 -0500

    Updated Style.css on signup, login

--- controllers/feRoutes.js
+++ controllers/feRoutes.js
@@ -1,24 +1,23 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.render('search', { h1value: 'User Registery' });
-});
-
-router.get('/fav', (req, res) => {
-    res.render('favorites', { h1value: 'favorites' });
-});
-
 router.get('/login', (req, res) => {
-    res.render('login', { h1value: 'login' });
-});
-
-router.get('/details', (req, res) => {
-    res.render('details', { h1value: 'details' });
+  res.render('login', { h1value: 'login' });
 });
 
 router.get('/signup', (req, res) => {
-    res.render('signup', { h1value: 'signup' });
+  res.render('signup', { h1value: 'signup' });
 });
 
+router.get('/', (req, res) => {
+  res.render('search', { h1value: 'User Registery' });
+});
+
+router.get('/fav', (req, res) => {
+  res.render('favorites', { h1value: 'favorites' });
+});
+
+router.get('/details', (req, res) => {
+  res.render('/details', { h1value: 'details' });
+});
 
 module.exports = router;
